bbf2c8b50b036356a0630160fe1505dd04ca09c2
mathru <user@mathru.net>
2023-01-11 11:20:38 +0900

    fix: Allow exports to be passed to deploy.

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,24 @@
 import * as data from "./lib/functions_data";
 import * as admin from "firebase-admin";
 export * from "./functions";
+export * from "./lib/functions_data";
 
 /**
  * Methods for deploying to Firebase Functions.
  * 
  * Firebase Functionsにデプロイするためのメソッドです。
  * 
- * @param deployFunctions
- * The elements defined in [Functions] are passed as an array. 
+ * @param exports
+ * Pass the `exports` as is.
  * 
- * The passed method is deployed. [Functions]で定義された要素を配列として渡します。渡されたメソッドがデプロイされます。
+ * `exports`をそのまま渡します。
+ * 
+ * @param deployFunctions
+ * The elements defined in [Functions] are passed as an array. The passed method is deployed.
+ * 
+ * [Functions]で定義された要素を配列として渡します。渡されたメソッドがデプロイされます。
  */
-export function deploy(deployFunctions: [data.FunctionsData]){
+export function deploy(exports: any, deployFunctions: [data.FunctionsData]){
     admin.initializeApp();
     for (const data of deployFunctions) {
         if (!process.env.FUNCTION_NAME || process.env.FUNCTION_NAME === data.id) {
