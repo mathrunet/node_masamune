1e5446294c73169e519e1d001dc1ba97ee9eea5c
mathru <user@mathru.net>
2023-09-29 18:39:33 +0900

    fix: Fixed a bug in the way parameters are taken.

--- src/functions/scheduler.ts
+++ src/functions/scheduler.ts
@@ -17,18 +17,18 @@ module.exports = (regions: string[], data: { [key: string]: string }) => functio
         try {
             const collectionPath = functions.config().scheduler.collection_path;
             const firestoreInstance = admin.firestore();
-            const collection = await firestoreInstance.collection(collectionPath).where("_done", "==", null).where("_command", "<=", Date.now()).orderBy("_command", "asc").get();
+            const collection = await firestoreInstance.collection(collectionPath).where("_done", "==", false).where("_command", "<=", Date.now()).orderBy("_command", "asc").get();
             for (var doc of collection.docs) {
                 const command = (doc.get("#command") as { [key: string]: any })["@command"];
                 const params = (doc.get("#command") as { [key: string]: any })["@params"] as { [key: string]: any };
                 switch (command) {
                     case "notification":
-                        const title = params.get("title") as string;
-                        const body = params.get("text") as string;
-                        const channelId = params.get("channel") as string | undefined;
-                        const data = params.get("data") as { [key: string]: any } | undefined;
-                        const token = params.get("token") as string | undefined;
-                        const topic = params.get("topic") as string | undefined;
+                        const title = params["title"] as string;
+                        const body = params["text"] as string;
+                        const channelId = params["channel"] as string | undefined;
+                        const data = params["data"] as { [key: string]: any } | undefined;
+                        const token = params["token"] as string | undefined;
+                        const topic = params["topic"] as string | undefined;
                         await sendNotification({
                             title: title,
                             body: body,
@@ -39,7 +39,7 @@ module.exports = (regions: string[], data: { [key: string]: string }) => functio
                         });
                         break;
                     case "copy_document":
-                        const path = params.get("path") as string;
+                        const path = params["path"] as string;
                         const id = path.split("/")[path.length - 1];
                         await firestoreInstance.doc(path).set(
                             {
