62913e7c84eb6f33b134cb4e0429e6245aaf3d50
mathru <user@mathru.net>
2023-10-20 16:27:11 +0900

    fix: Add ModelFieldValue for searchable.

--- src/lib/model_field_value.ts
+++ src/lib/model_field_value.ts
@@ -6,6 +6,35 @@
  */
 export class ModelFieldValue {
     /**
+     * Generates text data for Like search.
+     * 
+     * Like検索用のテキストデータを生成します。
+     * 
+     * @param {string} text
+     * Text to be searched.
+     * 
+     * 検索対象のテキスト。
+     *  
+     * @returns 
+     */
+    static searchable({
+        text,
+    }: {
+        text: string,
+    }) {
+        const res: { [key: string]: any } = {};
+        const search: { [key: string]: boolean } = {};
+        text.toZenkakuKatakana().toHiragana().toHankakuNumericAndAlphabet().toLowerCase().splitByCharacterAndBigram().forEach((e) => {
+            const trimed = e.trim();
+            if (trimed.length <= 0) {
+                return;
+            }
+            search[trimed] = true;
+        });
+        res["@search"] = search;
+        return res;
+    }
+    /**
      * Class for generating data for `ModelCounter`.
      * 
      * `ModelCounter`用のデータを生成するためのクラス。
