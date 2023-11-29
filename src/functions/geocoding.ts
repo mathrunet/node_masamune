b45997023ff0f2d763dd41fe16bf0263cac056cb
mathru <user@mathru.net>
2023-11-29 11:38:04 +0900

    fix: Config changes.

--- src/functions/geocoding.ts
+++ src/functions/geocoding.ts
@@ -6,7 +6,7 @@ import { Api } from "../lib/api";
  *
  * GeocodingAPIで緯度経度を取得します。
  *
- * @param {string} map.api_key
+ * @param {string} map.geocoding.api_key
  * API key for GoogleMapGeocodingAPI. Follow the steps below to issue it.
  * https://mathru.notion.site/Google-Map-API-API-e9a65fba9795450fb9a252ab4e631ace?pvs=4
  * GoogleMapGeocodingAPI用のAPIキー。下記の手順で発行します。
@@ -20,15 +20,16 @@ module.exports = (regions: string[], timeoutSeconds: number, data: { [key: strin
     async (query) => {
         try {
             const address = query.address;
-            const config = functions.config().map;
+            const config = functions.config().map.geocoding;
             const apiKey = config.api_key;
             if (!address) {
                 throw new functions.https.HttpsError("invalid-argument", "Query parameter is invalid.");
             }
             const res = await Api.get(
                 `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(address)}&key=${apiKey}`
-            );        
-            const json = (await res.json()) as { [key: string]: any };    
+            );
+            const json = (await res.json()) as { [key: string]: any };
+            console.log(json);
             return {
                 success: true,
                 ...json,
