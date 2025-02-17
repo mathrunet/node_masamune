d6302e50ab75243679b9ae0bbaa7108dfb8f888a
Masaru Hirose (mathru@Kanimiso-P) <45095843+mathrunet@users.noreply.github.com>
2025-02-17 23:39:04 +0900

    fix: Added the ability to output FieldValue of ModelLocale.

--- src/lib/model_field_value/model_field_value.ts
+++ src/lib/model_field_value/model_field_value.ts
@@ -110,4 +110,176 @@ export class ModelFieldValue {
         };
         return res;
     }
+    /**
+     * Class for generating data for `ModelDate`.
+     * 
+     * `ModelDate`用のデータを生成するためのクラス。
+     * 
+     * @param {string} key
+     * Data key.
+     * 
+     * データのキー。
+     * 
+     * @param {Date} date
+     * Date and time.
+     * 
+     * 日時。
+     * 
+     * @returns { [key: string]: any }
+     * Data for `ModelDate`.
+     * 
+     * `ModelDate`用のデータ。
+     */
+    static modelDate({
+        key, date,
+    }: {
+        key: string, date?: Date | undefined,
+    }): { [key: string]: any } {
+        const res: { [key: string]: any } = {};
+        date ??= new Date();
+        date.setHours(0, 0, 0, 0);
+        res[key] = date;
+        res[`#${key}`] = {
+            "@time": date.getTime(),
+            "@target": key,
+            "@type": "ModelDate",
+        };
+        return res;
+    }
+    /**
+     * Class for generating data for `ModelLocale`.
+     * 
+     * `ModelLocale`用のデータを生成するためのクラス。
+     * 
+     * @param {string} key
+     * Data key.
+     * 
+     * データのキー。
+     * 
+     * @param {string} language
+     * Language.
+     * 
+     * 言語。
+     * 
+     * @param {string} country
+     * Country.
+     * 
+     * 国。
+     * 
+     * @returns { [key: string]: any }
+     * Data for `ModelLocale`.
+     * 
+     * `ModelLocale`用のデータ。
+     */
+    static modelLocale({
+        key, language, country,
+    }: {
+        key: string, language: string, country: string,
+    }): { [key: string]: any } {
+        const res: { [key: string]: any } = {};
+        res[key] = `${language}_${country}`;
+        res[`#${key}`] = {
+            "@country": country,
+            "@language": language,
+            "@target": key,
+            "@type": "ModelLocale",
+        };
+        return res;
+    }
+    /**
+     * Class for generating data for `ModelUri`.
+     * 
+     * `ModelUri`用のデータを生成するためのクラス。
+     * 
+     * @param {string} key
+     * Data key.
+     * 
+     * データのキー。
+     * 
+     * @param {string} uri
+     * URI.
+     * 
+     * @returns { [key: string]: any }
+     * Data for `ModelUri`.
+     * 
+     * `ModelUri`用のデータ。
+     */
+    static modelUri({
+        key, uri,
+    }: {
+        key: string, uri: string,
+    }): { [key: string]: any } {
+        const res: { [key: string]: any } = {};
+        res[key] = uri;
+        res[`#${key}`] = {
+            "@target": key,
+            "@type": "ModelUri",
+            "@uri": uri,
+        };
+        return res;
+    }
+    /**
+     * Class for generating data for `ModelImageUri`.
+     * 
+     * `ModelImageUri`用のデータを生成するためのクラス。
+     * 
+     * @param {string} key
+     * Data key.
+     * 
+     * データのキー。
+     * 
+     * @param {string} uri
+     * URI.
+     * 
+     * @returns { [key: string]: any }
+     * Data for `ModelImageUri`.
+     * 
+     * `ModelImageUri`用のデータ。
+     */
+    static modelImageUri({
+        key, uri,
+    }: {
+        key: string, uri: string,
+    }): { [key: string]: any } {
+        const res: { [key: string]: any } = {};
+        res[key] = uri;
+        res[`#${key}`] = {
+            "@target": key,
+            "@type": "ModelImageUri",
+            "@uri": uri,
+        };
+        return res;
+    }
+    /**
+     * Class for generating data for `ModelVideoUri`.
+     * 
+     * `ModelVideoUri`用のデータを生成するためのクラス。
+     * 
+     * @param {string} key
+     * Data key.
+     * 
+     * データのキー。
+     * 
+     * @param {string} uri
+     * URI.
+     * 
+     * @returns { [key: string]: any }
+     * Data for `ModelVideoUri`.
+     * 
+     * `ModelVideoUri`用のデータ。
+     */
+    static modelVideoUri({
+        key, uri,
+    }: {
+        key: string, uri: string,
+    }): { [key: string]: any } {
+        const res: { [key: string]: any } = {};
+        res[key] = uri;
+        res[`#${key}`] = {
+            "@target": key,
+            "@type": "ModelVideoUri",
+            "@uri": uri,
+        };
+        return res;
+    }
 }
