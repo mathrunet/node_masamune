efbf6250228cb0fc3a19e462c1bf69491abbfbc2
Masaru Hirose (mathru@Kanimiso-P) <45095843+mathrunet@users.noreply.github.com>
2025-03-05 17:16:04 +0900

    fix: Functions options can be specified in detail.

--- src/functions/agora_cloud_recording.ts
+++ src/functions/agora_cloud_recording.ts
@@ -23,6 +23,7 @@ module.exports = (
         minInstances: options.minInstances,
         concurrency: options.concurrency,
         maxInstances: options.maxInstances,
+        serviceAccount: options.serviceAccount ?? undefined,
     },
     async (object) => {
         try {
