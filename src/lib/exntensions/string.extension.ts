c33a18f9baaa981f8b28cae1820aaa64fbce2767
Masaru Hirose (mathru@Kanimiso-P) <45095843+mathrunet@users.noreply.github.com>
2025-09-11 12:44:02 +0900

    fix: Removed unremovable emojis.

--- src/lib/exntensions/string.extension.ts
+++ src/lib/exntensions/string.extension.ts
@@ -416,7 +416,7 @@ String.prototype.splitByTrigram = function () {
 };
 
 String.prototype.removeOnlyEmoji = function () {
-    return (this as String).valueOf().replace(/[\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu, '');
+    return (this as String).valueOf().replace(/[\u{1F100}-\u{1F1FF}\u{1F600}-\u{1F64F}\u{1F300}-\u{1F5FF}\u{1F680}-\u{1F6FF}\u{1F700}-\u{1F77F}\u{1F780}-\u{1F7FF}\u{1F800}-\u{1F8FF}\u{1F900}-\u{1F9FF}\u{1FA00}-\u{1FA6F}\u{1FA70}-\u{1FAFF}\u{2600}-\u{26FF}\u{2700}-\u{27BF}]/gu, '');
 };
 
 String.prototype.toFirestoreMapKey = function () {
