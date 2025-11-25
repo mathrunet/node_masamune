7ce61feae9829bd7e8d969ef967f39f3f9ba0cfe
Masaru Hirose (mathru@Kanimiso-P) <45095843+mathrunet@users.noreply.github.com>
2025-11-25 13:28:32 +0900

    feat: Create a market pipeline.

---
+++ packages/masamune_marketing_pipeline/src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * Copyright (c) 2025 mathru. All rights reserved.
+ * 
+ * Masamune framework package plugin for market research and AI development.
+ * 
+ * To use, import * as m from "@mathrunet/masamune_marketing_pipeline";
+ *
+ * [mathru.net]: https://mathru.net
+ * [YouTube]: https://www.youtube.com/c/mathrunetchannel
+ */
+export * from "@mathrunet/masamune";
+export * from "./functions";
