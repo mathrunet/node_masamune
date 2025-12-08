9c0d1660ca60cbc72c7b7f742138352470a6e54c
Masaru Hirose (mathru@Kanimiso-P) <45095843+mathrunet@users.noreply.github.com>
2025-12-08 21:06:21 +0900

    feat: Delete the Katana package.

--- packages/katana/src/lib/regions.ts
+++
@@ -1,7 +0,0 @@
-
-/**
- * Define the Region.
- * 
- * Regionの定義を行います。
- */
-export type Regions = ("us-central1" | "us-east1" | "us-east4" | "europe-west1" | "europe-west2" | "europe-west3" | "asia-east2" | "asia-northeast1")[];
