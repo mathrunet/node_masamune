3eb5a6a06afb7e12c6a39cbce5a2328c70eb74d5
Masaru Hirose (mathru@Kanimiso-P) <45095843+mathrunet@users.noreply.github.com>
2025-12-11 12:24:30 +0900

    feat: Add JobRunner.

--- packages/masamune_workflow/src/lib/workflow_job_runner_function_base.ts
+++ packages/masamune_workflow/src/lib/workflow_job_runner_function_base.ts
@@ -3,6 +3,7 @@ import { firestoreLoader, FunctionsBase, HttpFunctionsOptions, ModelTimestamp } 
 import * as admin from "firebase-admin";
 import { Action, Task, Usage, Plan, Subscription, Campaign, TaskLog } from "./interfaces";
 import { GoogleGenAI } from "@google/genai";
+import { JobsClient } from "@google-cloud/run";
 import "@mathrunet/masamune";
 
 /**
@@ -41,19 +42,25 @@ export abstract class WorkflowJobRunnerFunctionBase extends FunctionsBase {
      */
     build(regions: string[]): Function {
         const options = this.options as HttpFunctionsOptions | undefined | null;
-        return functions.https.onCall(
+        return functions.tasks.onTaskDispatched(
             {
-                region: options?.region ?? regions,
-                timeoutSeconds: options?.timeoutSeconds ?? 540, // 9 minutes
-                memory: options?.memory ?? "1GiB",
+                timeoutSeconds: options?.timeoutSeconds,
+                memory: options?.memory,
                 minInstances: options?.minInstances,
                 concurrency: options?.concurrency,
                 maxInstances: options?.maxInstances,
                 serviceAccount: options?.serviceAccount ?? undefined,
-                enforceAppCheck: options?.enforceAppCheck ?? undefined,
-                consumeAppCheckToken: options?.consumeAppCheckToken ?? undefined,
             },
-            async (query) => {
+            async (request) => {
+                const projectId = process.env.GCLOUD_PROJECT || process.env.GOOGLE_CLOUD_PROJECT;
+                let region = options?.region ?? regions;
+                if (Array.isArray(region)) {
+                    region = region[0];
+                }
+                const jobsClient = new JobsClient();
+                await jobsClient.runJob({
+                    name: `projects/${projectId}/locations/${region}/jobs/${this.id}`,
+                });
             }
         );
     }
